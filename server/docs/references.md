89541b25ba3a3db1a5d3761b9dbdf51bde745b6a
Massimo Manfredino <massimo.manfredino@sourcesense.com>
2017-03-12 19:48:15 +0100

    Aggiunta di un test di tipo Hello World

--- server/docs/references.md
+++ server/docs/references.md
@@ -19,3 +19,12 @@ Neo4J - GraphQL integration
 
 https://github.com/neo4j-contrib/neo4j-graphql
 
+---
+Sample code
+
+How to create a basic read / write REST API
+http://davidandsuzi.com/writing-a-basic-api-with-graphql/
+
+How to create a GraphQL server in Java and a Javascript client
+https://nwillc.wordpress.com/2016/10/13/graphql-java-server-javascript-client/
+
---
+++ server/src/test/java/org/mjhost/HelloGraphQLTest.java
@@ -0,0 +1,35 @@
+package org.mjhost;
+
+import graphql.GraphQL;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
+import org.junit.Test;
+
+import static graphql.Scalars.GraphQLString;
+import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
+import static graphql.schema.GraphQLObjectType.newObject;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class HelloGraphQLTest {
+
+    @Test
+    public void testHelloWorld() {
+        GraphQLObjectType queryType = newObject()
+            .name("helloWorldQuery")
+            .field(
+                newFieldDefinition()
+                .type(GraphQLString)
+                .name("hello")
+                .staticValue("world")
+            )
+            .build();
+
+        GraphQLSchema schema = GraphQLSchema.newSchema()
+            .query(queryType)
+            .build();
+
+        Object result = new GraphQL(schema).execute("{hello}").getData();
+        assertThat(result.toString(), equalTo("{hello=world}"));
+    }
+}
